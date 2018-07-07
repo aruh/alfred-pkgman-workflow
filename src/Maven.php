b336eb16c3206d77f241e2e55e384e157687a366
Alexander Ruhland <alexander.ruhland@intive.com>
2018-07-07 17:57:42 +0200

    pass dependency string to workflow for copying

--- src/Maven.php
+++ src/Maven.php
@@ -6,47 +6,48 @@ require_once('Repo.php');
 
 class Maven extends Repo
 {
-    protected $id         = 'maven';
-    protected $kind       = 'libraries';
-    protected $url        = 'http://search.maven.org';
-    protected $search_url = 'http://search.maven.org/#search%7Cga%7C1%7C';
+	protected $id         = 'maven';
+	protected $kind       = 'libraries';
+	protected $url        = 'http://search.maven.org';
+	protected $search_url = 'http://search.maven.org/#search%7Cga%7C1%7C';
 
-    public function search($query)
-    {
-        if (!$this->hasMinQueryLength($query)) {
-            return $this->xml();
-        }
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
+			return $this->xml(); 
+		}
+		
+		$this->pkgs = $this->cache->get_query_json(
+			$this->id,
+			$query,
+			"{$this->url}/solrsearch/select?q={$query}&rows=10&wt=json"
+		)->response->docs;
+		
+		foreach($this->pkgs as $pkg) {
+			
+			// make params
+			$title = "{$pkg->a} (v{$pkg->latestVersion})";
+			$url = "{$this->url}/#artifactdetails%7C{$pkg->g}%7C{$pkg->a}%7C{$pkg->latestVersion}%7C{$pkg->p}";
+			$details = "GroupId: {$pkg->id}";
+	
+			$this->cache->w->result(
+				$pkg->a,
+				$this->makeArg($pkg->a, $url, "{$pkg->id}:{$pkg->latestVersion}"),
+				$title,
+				$details,
+				"icon-cache/{$this->id}.png"
+			);
+			
+			// only search till max return reached
+			if ( count ( $this->cache->w->results() ) == $this->max_return ) {
+				break;
+			}
+		}
 
-        $this->pkgs = $this->cache->get_query_json(
-            $this->id,
-            $query,
-            "{$this->url}/solrsearch/select?q={$query}&rows=10&wt=json"
-        )->response->docs;
+		$this->noResults($query, "{$this->search_url}{$query}");
 
-        foreach ($this->pkgs as $pkg) {
-            // make params
-            $title = "{$pkg->a} (v{$pkg->latestVersion})";
-            $url = "{$this->url}/#artifactdetails%7C{$pkg->g}%7C{$pkg->a}%7C{$pkg->latestVersion}%7C{$pkg->p}";
-            $details = "GroupId: {$pkg->id}";
-
-            $this->cache->w->result(
-                $pkg->a,
-                $this->makeArg($pkg->a, $url),
-                $title,
-                $details,
-                "icon-cache/{$this->id}.png"
-            );
-
-            // only search till max return reached
-            if (count($this->cache->w->results()) == $this->max_return) {
-                break;
-            }
-        }
-
-        $this->noResults($query, "{$this->search_url}{$query}");
-
-        return $this->xml();
-    }
+		return $this->xml();
+	}
 }
 
 // Test code, uncomment to debug this script from the command-line
